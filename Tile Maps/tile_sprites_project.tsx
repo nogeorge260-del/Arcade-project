d0b05a1a099b7e00da318edb5b8eb2a09c053f30
notonich <lyamin.dv1981@gmail.com>
2026-01-30 17:13:35 +0300

    Добавлено все потерянное.

---
+++ Tile Maps/tile_sprites_project.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tile_sprites_project" tilewidth="70" tileheight="70" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="just_bricks_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="just_grass.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="just_overgrown_brick_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="painful_death.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="plate_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="plate_mid.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="Plate_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="keyYellow.png" width="70" height="70"/>
+ </tile>
+ <tile id="8">
+  <image source="painful_death2.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="plate_left2.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="plate_mid2.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="Plate_right2.png" width="32" height="32"/>
+ </tile>
+</tileset>
